0a3a0177d927fe63bee8bf64182dc143504d2e8f
r3n33 <renee1382@gmail.com>
2024-05-11 11:25:52 -0500

    Adjust value output

--- views/view-statistics.lisp
+++ views/view-statistics.lisp
@@ -48,7 +48,7 @@
         (if (> stats-km 0.0) (setq whkm (/ (- stats-wh stats-wh-chg) stats-km)))
         (txt-block-l buf-efficiency 1 0 0 font18 (list "Wh/km" (if (> whkm 10.0)
             (str-from-n (to-i whkm) "%d")
-            (str-from-n whkm "%0.1f")
+            (str-from-n whkm "%0.2f")
         )))
 
         (txt-block-l buf-trip 1 0 0 font18 (list "Trip" (str-from-n stats-km (if (> stats-km 99.9) "%0.0fkm" "%0.1fkm"))))
@@ -74,7 +74,7 @@
     (var y-offs 10)
 
     (if (not-eq stats-amps-now (first view-previous-stats)) {
-        ; Max Amps Regen0
+        ; Max Amps Regen
         (img-clear buf-gauge)
         (draw-gauge-quadrant buf-gauge radius radius radius 1 2 17 0 (if (and (< stats-amps-now 0) (> stats-amps-max 0)) (/ (to-float (abs stats-amps-now)) stats-amps-max) 0) true true nil 4)
         (txt-block-r buf-gauge 3 radius 52 font18 (list (if (< stats-amps-now 0) (str-from-n stats-amps-now "%dA") "0A") (str-from-n stats-amps-now-min "%dA")))
@@ -93,13 +93,23 @@
         ; Watt Hours Consumed
         (img-clear buf-gauge)
         (draw-gauge-quadrant buf-gauge 0 0 radius 1 2 17 2 (if (> stats-wh 0) (/ stats-wh (+ stats-wh stats-wh-chg)) 0) true nil nil nil)
-        (txt-block-l buf-gauge 3 4 4 font18 (list (str-from-n stats-wh "%dWh") "out"))
+        (var wh-out-str (cond
+            ((< stats-wh 10.0) (str-from-n stats-wh "%0.1fWh"))
+            ((< stats-wh 1000.0) (str-from-n (to-i stats-wh) "%dWh"))
+            (_ (str-from-n (/ stats-wh 1000) "%0.1fkWh"))
+        ))
+        (txt-block-l buf-gauge 3 4 4 font18 (list wh-out-str "out"))
         (disp-render buf-gauge (+ padding (+ x-offs radius)) (+ padding (+ y-offs radius)) '(0x000000 0xfbd00a 0x1b1b1b 0xfbfcfc))
 
         ; Watt Hours Regenerated
         (img-clear buf-gauge)
         (draw-gauge-quadrant buf-gauge radius 0 radius 1 2 17 3 (if (> stats-wh-chg 0) (/ stats-wh-chg (+ stats-wh stats-wh-chg)) 0) nil nil nil nil)
-        (txt-block-r buf-gauge 3 radius 4 font18 (list (str-from-n stats-wh-chg "%dWh") "in"))
+        (var wh-in-str (cond
+            ((< stats-wh-chg 10.0) (str-from-n stats-wh-chg "%0.1fWh"))
+            ((< stats-wh-chg 1000.0) (str-from-n (to-i stats-wh-chg) "%dWh"))
+            (_ (str-from-n (/ stats-wh-chg 1000)"%0.1fkWh"))
+        ))
+        (txt-block-r buf-gauge 3 radius 4 font18 (list wh-in-str "in"))
         (disp-render buf-gauge x-offs (+ padding (+ y-offs radius)) '(0x000000 0x97bf0d 0x1b1b1b 0xfbfcfc))
     })
 

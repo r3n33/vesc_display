e579df9714f5a6c35b669498bd1710165b3e99c9
r3n33 <renee1382@gmail.com>
2024-06-15 13:42:27 -0500

    Progress on profile editing

--- views/view-profile-edit.lisp
+++ views/view-profile-edit.lisp
@@ -6,12 +6,31 @@
         (def state-view-next 'view-profile-select)
     })
 
-    ; TODO: select previous option (wrap to end)
-    ; TODO: select next option (wrap to start)
+    ; Select previous option (wrap to end)
+    (defun on-btn-1-pressed () {
+        (if (eq profile-edit-item 2)
+            (setq profile-edit-item-next 0)
+            (setq profile-edit-item-next (+ profile-edit-item 1))
+        )
+    })
+    ; Select next option (wrap to start)
+    (defun on-btn-2-pressed () {
+        (if (eq profile-edit-item 0)
+            (setq profile-edit-item-next 2)
+            (setq profile-edit-item-next (- profile-edit-item 1))
+        )
+    })
 
     (defun on-btn-3-pressed () {
         (setq view-profile-mode 'edit-option)
         (set-menu-edit-option)
+
+        (def profile-edit-value (match profile-edit-item
+            (0 (read-setting (str2sym (str-from-n view-profile-index "pf%d-speed"))))
+            (1 (read-setting (str2sym (str-from-n view-profile-index "pf%d-current"))))
+            (2 (read-setting (str2sym (str-from-n view-profile-index "pf%d-wattage"))))
+        ))
+        (def profile-edit-value-previous nil)
     })
 
     ; Render menu
@@ -24,10 +43,25 @@
     (defun on-btn-0-pressed () {
         (setq view-profile-mode 'select-option)
         (set-menu-select-option)
+        ; TODO: remove highlight from selected option
     })
 
     ; TODO: decrease value
+    (defun on-btn-1-pressed () {
+        (match profile-edit-item
+            (0 nil)
+            (1 nil)
+            (2 nil)
+        )
+    })
     ; TODO: increase value
+    (defun on-btn-2-pressed () {
+        (match profile-edit-item
+            (0 nil)
+            (1 nil)
+            (2 nil)
+        )
+    })
 
     (defun on-btn-3-pressed () {
         ; TODO: save
@@ -39,13 +73,21 @@
 })
 
 (defun view-init-profile-edit () {
+    (def view-profile-index (+ profile-active 1)) ; TODO: use persistent setting
+
     (var buf-title (img-buffer 'indexed4 240 30))
     (txt-block-r buf-title (list 0 1 2 3) 240 0 font18
-        (str-from-n (+ profile-active 1) "Edit Profile %d") ; TODO: use persistent setting
+        (str-from-n view-profile-index "Edit Profile %d")
     )
     (disp-render buf-title 80 4 '(0x000000 0x4f514f 0x929491 0xfbfcfc))
 
-    (def profile-edit-item 0)
+    (def profile-edit-item nil)
+    (def profile-edit-item-next 0)
+    (def profile-edit-value nil)
+    (def profile-edit-value-previous nil)
+    (def buf-profile-opt0 (img-buffer 'indexed4 290 30))
+    (def buf-profile-opt1 (img-buffer 'indexed4 290 30))
+    (def buf-profile-opt2 (img-buffer 'indexed4 290 30))
 
     (def view-profile-mode 'select-option)
     (set-menu-select-option)
@@ -53,19 +95,83 @@
 
 (defun view-draw-profile-edit () {
     (match view-profile-mode
-        ('select-option {
+        (select-option {
+            (if (not-eq profile-edit-item profile-edit-item-next) {
+                ; Draw saved values
+                (txt-block-l buf-profile-opt0 (list 0 1 2 3) 0 0 font24
+                    (str-merge (str-from-n (to-i (* (read-setting (str2sym (str-from-n view-profile-index "pf%d-speed"))) 100)) "Speed %d") "%")
+                )
 
+                (txt-block-l buf-profile-opt1 (list 0 1 2 3) 0 0 font24
+                    (str-merge (str-from-n (to-i (* (read-setting (str2sym (str-from-n view-profile-index "pf%d-current"))) 100)) "Current %d") "%")
+                )
+
+                (txt-block-l buf-profile-opt2 (list 0 1 2 3) 0 0 font24
+                    (str-from-n (read-setting (str2sym (str-from-n view-profile-index "pf%d-wattage"))) "%d Watts")
+                )
+            })
         })
-        ('edit-option {
-            
+        (edit-option {
+            (if (not-eq profile-edit-value profile-edit-value-previous) {
+                (match profile-edit-item
+                    (0
+                        (txt-block-l buf-profile-opt0 (list 0 1 2 3) 0 0 font24
+                            (str-merge (str-from-n (to-i (* profile-edit-value 100)) "Speed %d") "%")
+                        )
+                    )
+                    (1
+                        (txt-block-l buf-profile-opt1 (list 0 1 2 3) 0 0 font24
+                            (str-merge (str-from-n (to-i (* profile-edit-value 100)) "Current %d") "%")
+                        )
+                    )
+                    (2
+                        (txt-block-l buf-profile-opt2 (list 0 1 2 3) 0 0 font24
+                            (str-from-n profile-edit-value "%d Watts")
+                        )
+                    )
+                )
+            })
         })
     )
 })
 
+(defun menu-option-color (index highlight-index selected-index) {
+    (var colors-menu '( 0x000000 0x4f514f 0x929491 0xfbfcfc))
+    (var colors-menu-highlight '( 0x000000 0x113f60 0x1e659a 0x1d9aed))
+    (var colors-menu-selected '( 0x000000 0x006107 0x039e06 0x0df412))
+
+    (cond
+        ((eq index selected-index) colors-menu-selected)
+        ((eq index highlight-index) colors-menu-highlight)
+        (_ colors-menu)
+    )
+})
+
 (defun view-render-profile-edit () {
-    
+    (match view-profile-mode
+        (select-option {
+            (if (not-eq profile-edit-item profile-edit-item-next) {
+                ; Render
+                (disp-render buf-profile-opt0 5 35  (menu-option-color 0 profile-edit-item-next -1))
+                (disp-render buf-profile-opt1 5 95 (menu-option-color 1 profile-edit-item-next -1))
+                (disp-render buf-profile-opt2 5 155 (menu-option-color 2 profile-edit-item-next -1))
+                (setq profile-edit-item profile-edit-item-next)
+            })
+        })
+        (edit-option {
+            (if (not-eq profile-edit-value profile-edit-value-previous) {
+                ; Render
+                (disp-render buf-profile-opt0 5 35  (menu-option-color 0 -1 profile-edit-item))
+                (disp-render buf-profile-opt1 5 95  (menu-option-color 1 -1 profile-edit-item))
+                (disp-render buf-profile-opt2 5 155 (menu-option-color 2 -1 profile-edit-item))
+                (setq profile-edit-value-previous profile-edit-value)
+            })
+        })
+    )
 })
 
 (defun view-cleanup-profile-edit () {
-    
+    (def buf-profile-opt0 nil)
+    (def buf-profile-opt1 nil)
+    (def buf-profile-opt2 nil)
 })

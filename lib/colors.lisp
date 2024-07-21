4e41cd6ed5d7748f8299b2355d9cdfa435a3437f
r3n33 <renee1382@gmail.com>
2024-07-21 15:45:36 -0500

    Hide and show background stripes with sidestand state

--- lib/colors.lisp
+++ lib/colors.lisp
@@ -5,4 +5,21 @@
 (def colors-blue-icon '(0x000000 0x020040 0x000077 0x1d00e8))
 (def colors-dim-icon '(0x000000 0x090909 0x101010 0x171717))
 (def colors-white-icon '(0x000000 0x4f514f 0x929491 0xfbfcfc))
+(def colors-stripes-16c '(  0x000000
+                            0x1d9af7 ; top fg
+                            0x1574b6 ; 2
+                            0x0e5179 ; 3
+                            0x143e59 ; 4
+                            0x0e222f ; 5
+                            0x00c7ff ; bottom fg
+                            0x10b2e6 ; 7
+                            0x1295bf ; 8
+                            0x0984ac ; 9
+                            0x007095 ; a
+                            0x0e5179 ; b
+                            0x08475c ; c
+                            0x143e59 ; d
+                            0x0e222f ; e
+                        ))
+
 @const-end
